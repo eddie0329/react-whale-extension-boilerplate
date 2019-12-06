d30ab68d810186db34cf074d556b3b7e2476294e
eddie0329 <fdsa0329@naver.com>
2019-12-07 02:25:01 +0900

    whale extension sidebar template initiated

---
+++ src/pages/Sidebar/Sidebar.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export class Sidebar extends Component {
+  render() {
+    return <div>this is sidebar</div>;
+  }
+}
+
+export default Sidebar;
--- webpack.config.js
+++ webpack.config.js
@@ -34,11 +34,9 @@ if (fileSystem.existsSync(secretsPath)) {
 var options = {
   mode: process.env.NODE_ENV || 'development',
   entry: {
-    newtab: path.join(__dirname, 'src', 'pages', 'Newtab', 'index.jsx'),
-    options: path.join(__dirname, 'src', 'pages', 'Options', 'index.jsx'),
-    popup: path.join(__dirname, 'src', 'pages', 'Popup', 'index.jsx'),
     background: path.join(__dirname, 'src', 'pages', 'Background', 'index.js'),
     contentScript: path.join(__dirname, 'src', 'pages', 'Content', 'index.js'),
+    sidebar: path.join(__dirname, 'src', 'pages', 'Sidebar', 'index.jsx'),
   },
   chromeExtensionBoilerplate: {
     notHotReload: ['contentScript'],
@@ -123,19 +121,9 @@ var options = {
       }
     ),
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src', 'pages', 'Newtab', 'index.html'),
-      filename: 'newtab.html',
-      chunks: ['newtab'],
-    }),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src', 'pages', 'Options', 'index.html'),
-      filename: 'options.html',
-      chunks: ['options'],
-    }),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src', 'pages', 'Popup', 'index.html'),
-      filename: 'popup.html',
-      chunks: ['popup'],
+      template: path.join(__dirname, 'src', 'pages', 'Sidebar', 'index.html'),
+      filename: 'sidebar.html',
+      chunks: ['sidebar'],
     }),
     new HtmlWebpackPlugin({
       template: path.join(
